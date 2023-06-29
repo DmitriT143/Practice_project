2aebe08cd988eb2e6ae177c81973b021daad1da6
user <email>
2023-06-29 15:34:44 +0300

    Initial commit to working part

---
+++ App/Working part.py
@@ -0,0 +1,14 @@
+import random
+
+
+def initial_roll(ammount, max_value):
+    def dice_roll(max_value):
+        dice = random.randint(1, max_value)
+        print(dice)
+        return dice
+    for i in range(ammount):
+        i = i + 1
+        dice_roll(max_value)
+
+
+initial_roll(2,20)
